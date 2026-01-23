28d7b795a0e4730426fadb88ca2c95b5ffcb73c0
LuffyXoro <rudra.raghav1212@gmail.com>
2026-01-23 20:30:53 +0530

    run

--- run_pipeline.py
+++ run_pipeline.py
@@ -3,6 +3,10 @@
 
 from data_collection.yt_api import fetch_comments
 from preprocessing.clean_text import json_to_dataframe, clean_text
-from sentiment.model import 
-from analysis.trend_analysis import keyword_frequency
+from sentiment.model import predict_sentiment
+from analysis.trend_analysis import (keyword_frequency,
+                                        keyword_by_sentiment)
+
+
+
 
--- sentiment/model.py
+++ sentiment/model.py
@@ -71,3 +71,4 @@ def predict_sentiment(text):      # input:cleaned text ; output:semtiment label 
 
 
 
+
